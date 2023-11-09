14fe31a36f6cbf1fcc895d645acda588dfe87d50
monson2002 <monsonreji2002@gmail.com>
2023-11-09 11:34:55 +0530

    Remove unused function r_key_store

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-r_key_store(self, username):
-        dic = {}
-        dic["KMS"] = self.generate_key
-        for key,value in self.user_keys.items():
-            if key == username:
-                pass
-            else:
-                dic[key] = value
-        return dic
